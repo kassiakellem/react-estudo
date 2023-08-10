c8088b333d7d7d7b247c71998fb0076934750ffb
Kassiakellem <kassiakellem4@gmail.com>
2023-08-10 00:08:31 -0300

    começo do estudo em react da alura

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Banner from './Componentes/Banner/banner';
 
 function App() {
   return (
     <div className="App">
+      <Banner/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> Bem-vindo ao react.
         </p>
         <a
           className="App-link"
